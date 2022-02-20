6f0924925bfc8a01a24c8f3cdd1d10ceb77a5570
Jakkarin100700 <80670958+Jakkarin100700@users.noreply.github.com>
2022-02-20 19:47:07 +0700

    first commit

---
+++ miniproject/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import SearchBox from './Homes/Searchmy';
+//import Account from './Homes/Account';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/Style.css';
+
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<SearchBox />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
